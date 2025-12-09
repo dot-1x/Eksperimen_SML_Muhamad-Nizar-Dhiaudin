ed4bf15a7fe8db6da95f4d9536dd75da94fe4e44
dot-1x <mugiwaraluffy2003@gmail.com>
2025-12-09 16:59:29 +0700

    feat (preprocessing): added rfm preprocessing

--- preprocessing/automate_Muhamad-Nizar-Dhiaudin.py
+++ preprocessing/automate_Muhamad-Nizar-Dhiaudin.py
@@ -1,4 +1,5 @@
 import warnings
+from datetime import timedelta
 
 import pandas as pd
 import seaborn as sns
@@ -27,40 +28,71 @@ def replace_with_threshold(dataframe, variable):
 
 
 def preprocess(df):
-  # dropna
-  print("Removing missing values...")
-  df.dropna(subset="Customer ID",axis=0,inplace=True)
-  # removing canceled product
-  print("Removing canceled transactions...")
-  df = df[~df.Invoice.str.contains('C',na=False)]
-  # drop duplicates
-  print("Removing duplicate records...")
-  df = df.drop_duplicates()
-  # Convert InvoiceDate to datetime
-  print("Converting InvoiceDate to datetime...")
-  df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
-  # Handling negative values
-  print("Removing negative values in Quantity and Price...")
-  df = df[(df['Quantity'] > 0) & (df['Price'] > 0)]
-  # Data type validation
-  print("Validating and converting data types...")
-  df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
-  df['Quantity'] = pd.to_numeric(df['Quantity'], errors='coerce')
-  df = df.dropna(subset=['Price', 'Quantity'])
-  # Remove empty strings and whitespace
-  print("Removing empty strings and whitespace in key columns...")
-  df['Description'] = df['Description'].str.strip()
-  df = df[df['Description'].str.len() > 0]
-  # Remove zero values in critical columns
-  print("Removing zero values in Quantity and Price...")
-  df = df[(df['Quantity'] != 0) & (df['Price'] != 0)]
-  # removing outlier
-  print("Handling outliers in Quantity and Price columns...")
-  replace_with_threshold(df,"Quantity")
-  replace_with_threshold(df,"Price")
-  # Feature engineering: create Revenue column
-  df["Revenue"] = df["Quantity"] * df["Price"]
-  return df
+    # dropna
+    print("Removing missing values...")
+    df.dropna(subset="Customer ID",axis=0,inplace=True)
+    # removing canceled product
+    print("Removing canceled transactions...")
+    df = df[~df.Invoice.str.contains('C',na=False)]
+    # drop duplicates
+    print("Removing duplicate records...")
+    df = df.drop_duplicates()
+    # Convert InvoiceDate to datetime
+    print("Converting InvoiceDate to datetime...")
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+    # Handling negative values
+    print("Removing negative values in Quantity and Price...")
+    df = df[(df['Quantity'] > 0) & (df['Price'] > 0)]
+    # Data type validation
+    print("Validating and converting data types...")
+    df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
+    df['Quantity'] = pd.to_numeric(df['Quantity'], errors='coerce')
+    df = df.dropna(subset=['Price', 'Quantity'])
+    # Remove empty strings and whitespace
+    print("Removing empty strings and whitespace in key columns...")
+    df['Description'] = df['Description'].str.strip()
+    df = df[df['Description'].str.len() > 0]
+    # Remove zero values in critical columns
+    print("Removing zero values in Quantity and Price...")
+    df = df[(df['Quantity'] != 0) & (df['Price'] != 0)]
+    # removing outlier
+    print("Handling outliers in Quantity and Price columns...")
+    replace_with_threshold(df,"Quantity")
+    replace_with_threshold(df,"Price")
+    # Feature engineering: create Revenue column
+    df["Revenue"] = df["Quantity"] * df["Price"]
+    return df
+
+
+def rfm_featuring(data: pd.DataFrame):
+    """
+    Creates RFM features from the input data.
+
+    Parameters:
+    data (pd.DataFrame): The input dataframe.
+
+    Returns:
+    pd.DataFrame: Dataframe with RFM features.
+    """
+    # Mengatur tanggal terakhir 2011-12-10 sebagai invoice date terakhir menjadi 2011-12-09.
+    data["InvoiceDate"] = pd.to_datetime(data["InvoiceDate"])
+    latest_date = data["InvoiceDate"].max() + timedelta(days=1)
+    # Membuat RFM features berdasarkan subset dari customerID
+    RFM = data.groupby("Customer ID").agg(
+        {
+            "InvoiceDate": lambda x: (latest_date - x.max()).days, # hari sejak terakhir pembelian
+            "Invoice": lambda x: x.nunique(),
+            "Revenue": lambda x: x.sum()
+        }
+    )
+
+    RFM["InvoiceDate"] = RFM["InvoiceDate"].astype(int)
+    RFM.rename(columns={"InvoiceDate": "Recency", "Invoice": "Frequency", "Revenue": "Monetary"}, inplace=True)
+    Shopping_Cycle = data.groupby("Customer ID").agg({"InvoiceDate": lambda x: ((x.max() - x.min()).days)})
+    RFM["Shopping_Cycle"] = Shopping_Cycle
+    RFM["Interpurchase_Time"] = RFM["Shopping_Cycle"] // RFM["Frequency"]
+    
+    return RFM[["Recency", "Frequency", "Monetary", "Interpurchase_Time"]]
 
 
 def main():
@@ -72,8 +104,10 @@ def main():
 
     # Preprocess data
     df_cleaned = preprocess(df)
+    # Feature engineering: RFM features
+    print("Creating RFM features...")
+    df_cleaned = rfm_featuring(df_cleaned)
 
-    # Save cleaned data
     print("Saving cleaned data to './preprocessing/uci-retail_preprocessing.csv'...")
     df_cleaned.to_csv('./preprocessing/uci-retail_preprocessing.csv', index=False)
 
